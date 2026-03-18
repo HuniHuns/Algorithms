5c744423ba61edab60d262ab9b2e3bab0c0c382a
LEEJAEHUN <i690367@gmail.com>
2026-03-18 18:27:39 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.py
@@ -0,0 +1,21 @@
+""" solve.py for 1000. A+B """
+
+import sys
+
+
+def sys_input() -> str:
+    return sys.stdin.readline().rstrip()
+
+
+def solve(a: int, b: int) -> int:
+    return a+b
+
+
+def main() -> None:
+    a, b = map(int, sys_input().split())
+    answer:int = solve(a, b)
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
